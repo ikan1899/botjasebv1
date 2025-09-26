4392cabefa50537dcafc2b2237b1e76149931c24
ikan1899 <ganjarslewbewww@gmail.com>
2025-09-26 15:37:09 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  BOT_TOKEN: "8247012653:AAFmvkA6CiSjbDSFRdl0MKzzlLwQK-cWWI0",
+  OWNER_IDS: ["7249549761"],
+  CHANNEL_USERNAME: "@gudel021",
+  DEVELOPER: "@gudel023",
+  VERSION: "1.7",
+  CHANNEL_URL: "https://t.me/gudel021",
+  SHARE_FOOTER: "\n\nBOT JASEB FRE✅️\n@BotJaseb2GudelBot\n@BotJaseb2GudelBot\n@BotJaseb2GudelBot",
+  MENU_IMAGES: [
+    "https://l.top4top.io/p_3545u9n2v1.jpg"
+  ]
+};
